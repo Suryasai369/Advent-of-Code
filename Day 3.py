63df5d83ecc9b8ab64e1aa50070c81c5405c7ebf
Suryasai369 <95473442+Suryasai369@users.noreply.github.com>
2025-02-17 12:52:39 +0530

    Day 3 complete

--- Day 3.py
+++ Day 3.py
@@ -3,31 +3,34 @@ def inp():
     with open("input.txt") as f:
         return f.readlines()
 data = str(inp())
-ans=0
-'''for i in range(len(data)-8):
-    if data[i]=='m' and data[i+1]=='u' and data[i+2]=="l" and data[i+3]=='(' and data[i+4].isnumeric() and data[i+5]==',' and data[i+6].isnumeric() and data[i+7]==')':
-        ans+=int(data[i+4])*int(data[i+6])
 
-'''
+#data="xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+def mul(data):
+    ans = 0
+    t = re.findall("mul\(\d*,\d*\)", data)
+    for i in t:
+        c=0
+        for j in range(len(i)):
+            if i[j]==",":
+                c=j
+        n1=int(i[4:c])
+        n2=int(i[c+1:len(i)-1])
+        ans+=n1*n2
+    return ans
 
-#data="xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-'''
-for i in range(len(data)-7):
-    if data[i]=='m' and data[i+1]=='u' and data[i+2]=="l" and data[i+3]=='(' and data[i+4].isnumeric() and data[i+5]==',' and data[i+6].isnumeric() and data[i+7]==')':
-        print("in")
-        print(data[i:i+8])
-        ans+=int(data[i+4])*int(data[i+6])
-    if i==53:
-        print(data[i:i+8])
-print(ans)
-'''
-t = re.findall("mul\(\d*,\d*\)", data)
-for i in t:
-    c=0
-    for j in range(len(i)):
-        if i[j]==",":
-            c=j
-    n1=int(i[4:c])
-    n2=int(i[c+1:len(i)-1])
-    ans+=n1*n2
-print(ans) 
+def dodn(data):
+    while True:
+        if re.search(r"don't\(\)", data) == None:
+            break
+        else:
+            donw=re.search(r"don't\(\)", data)
+            te=data[donw.end():]
+            doni=donw.end()
+            dow=re.search("do\(\)", te)
+            doi=dow.start()
+        data=data[:doni-7]+data[doni+doi+4:]
+    print(mul(data))
+            
+
+
+dodn(data)
